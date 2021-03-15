b8352e1f57012acf6d26cc2b65299d5bb2c01c53
CamilleLacroix <camille.lacroix.2@ulaval.ca>
2021-03-15 09:38:42 -0400

    def_1

---
+++ main.py
@@ -0,0 +1,22 @@
+# QPU base operations
+
+# 1. NOT
+''' fait un switch de valeur: |1> devient |0>, |0> devient |1>, 
+    90% de chance de retrouver |1> devient 10% de la même valeur
+    Réversible, appliquer 2 fois pour retourner à l'identité'''
+
+# 2. HAD
+''' Hadamar, crée un état de superposition: |0> devient 
+    |1/RAC(2)> phase = 0°, |1> devient |1/RAC(2)> phase /= 0°'''
+
+# 3. READ
+''' not est l'équivalent de l'opération 0, il transforme la
+    la valeur en 0'''
+
+# 4. WRITE
+''' not est l'équivalent de l'opération 0, il transforme la
+    la valeur en 0'''
+
+# 5. PHASE(teta)
+# 6. ROTX(teta) and ROTY(teta)
+# 7. ROOT of NOT
